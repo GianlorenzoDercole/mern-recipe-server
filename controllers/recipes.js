f58b8c6aaa2cbe212478f43d31d0fa7b9e31b4b2
GianlorenzoDercole <johndercole01@gmail.com>
2022-11-11 12:57:23 -0800

    post request working see recipes.js

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -1,6 +1,14 @@
-
 const db = require('../models')
-
 const router = require('express').Router()
+
+router.post('/', async (req, res) => {
+    try {
+        const newRecipe = await db.Recipe.create(req.body)
+        res.status(201).json(newRecipe)
+    } catch (err) {
+        console.log(err)
+        res.status(500).json(err)
+    }
+})
 
 module.exports = router
